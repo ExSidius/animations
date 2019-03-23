61994e8dde03a21889908d0579bcc7a6a527e26c
mram <mram@umd.edu>
2019-03-23 15:05:03 -0400

    Adding edges to balls

--- gravity/animation.js
+++ gravity/animation.js
@@ -56,6 +56,8 @@ class Ball {
 		context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
 		context.fillStyle = this.color
 		context.fill()
+		context.lineWidth = 0.5
+		context.stroke()
 		context.closePath()
 	}
 }
