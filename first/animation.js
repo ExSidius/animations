360b7d7dc4de54a755c474c8574085567d0b3178
mram <mram@umd.edu>
2019-03-23 13:29:42 -0400

    Black circles

--- first/animation.js
+++ first/animation.js
@@ -7,20 +7,22 @@ let context = canvas.getContext('2d')
 
 class Circle {
 	constructor() {
-		this.x = Math.random() * innerWidth
-		this.dx = Math.random() * 40 - 20
 
-		this.y = Math.random() * innerHeight
-		this.dy = Math.random() * 40 - 20
+		this.radius = Math.random() * 30 + 30
 
-		this.radius = Math.random() * 20 + 20
+		this.x = Math.random() * (innerWidth - this.radius * 2) + this.radius
+		this.dx = Math.random() * 20 - 10
+
+		this.y = Math.random() * (innerHeight - this.radius * 2) + this.radius
+		this.dy = Math.random() * 20 - 10
 	}
 
 	draw() {
 		context.beginPath()
 		context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-		context.strokeStyle = getRandomColor()
+		context.strokeStyle = 'blue'
 		context.stroke()
+		context.fill()
 	}
 
 	update() {
@@ -37,15 +39,6 @@ class Circle {
 
 		this.draw()
 	}
-}
-
-function getRandomColor() {
-  let letters = '0123456789ABCDEF'
-  let color = '#'
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]
-  }
-  return color;
 }
 
 let circles = [...Array(20)].map(_ => new Circle())
